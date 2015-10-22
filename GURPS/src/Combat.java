fa7175bd4b8e0f0f0dd23d19864b42dd65fc5696
Anton <anton.s.friedmann@gmail.com>
2015-10-22 19:41:48 +0200

    småfix

--- GURPS/src/Combat.java
+++ GURPS/src/Combat.java
@@ -1,26 +1,30 @@
 
 public class Combat {
 	static int totalRoll;
+	static int damageRoll;
 	//Gick på str pga att attackskill ej är implementerat men ville kunna göra koden möjlig ändå
 	//Jag har även ignorerat turordning tillsvidare
-	public static boolean attack(Character atk){
+	public static boolean attack(Character atk, Character def){
 		
 		totalRoll = 0;
 		for(int i=0; i<3 ; i++){
 			totalRoll = totalRoll + Die.roll();
 		}
 		
-		if(totalRoll <= atk.getStrength())
+		if(totalRoll <= atk.getStrength()){
+			defend(def);
 			return true;
+		}
 		else 
 			return false;
 	}
 	
-	//samma här som på attack. Har även gjort all damage till 1.
+	
 	//Är medveten om att dessa är identiska men då dessa atkskill samt defendskill behövs
 	public static boolean defend(Character def){
 		
 		totalRoll = 0;
+		
 		for(int i=0; i<3 ; i++){
 			totalRoll = totalRoll + Die.roll();
 		}
@@ -28,7 +32,8 @@ public class Combat {
 		if(totalRoll <= def.getStrength())
 			return true;
 		else {
-			def.setHealth(def.getHealth() - 1);
+			damageRoll=Die.roll();
+			def.setHealth(def.getHealth() - damageRoll);
 			return false;
 		}
 	}
--- GURPS/src/CombatTest.java
+++ GURPS/src/CombatTest.java
@@ -3,12 +3,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CombatTest {
-
+	Character atk = new Character("Per", 100);
+	Character def = new Character("olle", 100);
+	
 	@Test
 	public void attackTest() {
-		Character atk = new Character("Per", 100);
 		atk.setStrength(10);
-		boolean strike = Combat.attack(atk);
+		boolean strike = Combat.attack(atk, def);
 		int roll = Combat.totalRoll;
 		if (roll <= atk.getStrength())
 			assertEquals(strike, true);
@@ -25,7 +26,7 @@ public class CombatTest {
 		if (roll <= def.getStrength())
 			assertEquals(defend, true);
 		else{
-			assertEquals(def.getHealth(), 9);
+			assertEquals(def.getHealth(), 10-Combat.damageRoll);
 			assertEquals(defend, false);
 		}
 	}
