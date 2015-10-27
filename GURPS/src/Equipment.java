5d030d911cc05112aa35697027a4eda12167d193
Erik Sandsten <eriksandsten12@gmail.com>
2015-10-27 20:15:45 +0100

    Fixade "Dagger"-klass och lite annat

--- GURPS/src/Equipment.java
+++ GURPS/src/Equipment.java
@@ -25,15 +25,17 @@ public final class Equipment {
 		}
 	}
 	
-	public static class Armor extends Item {
+	public abstract static class Armor extends Item {
 		private int passiveDefense;
 		private int damageResistance;
+		private int techLevel;
 		
-		public Armor(String description, int value, double weight, int passiveDefense, int damageResistance) {
+		public Armor(String description, int value, double weight, int passiveDefense, int damageResistance, int techLevel) {
 			super(description, value, weight);
 			
 			this.passiveDefense = passiveDefense;
 			this.damageResistance = damageResistance;
+			this.techLevel = techLevel;
 		}
 		
 		public int getPassiveDefense() {
@@ -43,9 +45,23 @@ public final class Equipment {
 		public int getDamageResistance() {
 			return damageResistance;
 		}
+		
+		public int getTechLevel() {
+			return techLevel;
+		}
+		
+		public static class LeatherJacket extends Armor {
+			public LeatherJacket(int techLevel) {
+				super("Leather Jacket", 50, 4.0, 1, 1, techLevel);
+				
+				if (techLevel < 1 || techLevel > 8) {
+					throw new IllegalArgumentException();
+				}
+			}
+		}
 	}
 	
-	public static class Shield extends Item {
+	public abstract static class Shield extends Item {
 		private int passiveDefense;
 		
 		public Shield(String description, int value, double weight, int passiveDefense) {
@@ -56,6 +72,48 @@ public final class Equipment {
 		
 		public int getPassiveDefense() {
 			return passiveDefense;
+		}
+		
+		public static class Buckler extends Shield {
+			public Buckler() {
+				super("Buckler", 25, 2.0, 1);
+			}
+		}
+	}
+	
+	public abstract static class Weapon extends Item {
+		private int minimumStrength; // Minimum strength to wield the weapon
+		private int damageType;
+		
+		public static final int DAMAGE_TYPE_IMPALING = 1; // 001
+		public static final int DAMAGE_TYPE_CUTTING  = 2; // 010
+		public static final int DAMAGE_TYPE_CRUSHING = 4; // 100
+	
+		public Weapon(String description, int value, double weight, int minimumStrength, int damageType) {
+			super(description, value, weight);
+			
+			this.minimumStrength = minimumStrength;
+			this.damageType = damageType;
+		}
+		
+		public int getMinimumStrength() {
+			return minimumStrength;
+		}
+		
+		public int getDamageType() {
+			return damageType;
+		}
+		
+		public abstract static class HandWeapon extends Weapon {
+			public HandWeapon(String description, int value, double weight, int minimumStrength, int damageType) {
+				super(description, value, weight, minimumStrength, damageType);
+			}
+			
+			public static class Dagger extends HandWeapon {
+				public Dagger() {
+					super("Dagger", 20, 0.25, 0, DAMAGE_TYPE_IMPALING);
+				}
+			}
 		}
 	}
 }
--- GURPS/src/EquipmentTest.java
+++ GURPS/src/EquipmentTest.java
@@ -6,31 +6,55 @@ public class EquipmentTest {
 
 	@Test
 	public void itemConstructorTest() {
-		Equipment.Item ring = new Equipment.Item("Ring", 20, 0.01);
+		Equipment.Item ring = new Equipment.Item("Ring", 20, 0.0);
 		
 		assertEquals("Ring", ring.getDescription());
 		assertEquals(20, ring.getValue());
-		assertEquals(0.01, ring.getWeight());
+		assertEquals(0.0, ring.getWeight());
 	}
 	
 	@Test
-	public void armorConstructorTest() {
-		Equipment.Armor leatherJacket = new Equipment.Armor("Leather Jacket", 50, 4.0, 1, 1);
+	public void leatherJacketConstructorTest() {
+		Equipment.Armor.LeatherJacket leatherJacket = new Equipment.Armor.LeatherJacket(8);
 		
 		assertEquals("Leather Jacket", leatherJacket.getDescription());
 		assertEquals(50, leatherJacket.getValue());
 		assertEquals(4.0, leatherJacket.getWeight());
 		assertEquals(1, leatherJacket.getPassiveDefense());
 		assertEquals(1, leatherJacket.getDamageResistance());
+		assertEquals(8, leatherJacket.getTechLevel());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void leatherJacketTooHighTechLevelTest() {
+		// TechLevel should be 1-8
+		Equipment.Armor.LeatherJacket leatherJacket = new Equipment.Armor.LeatherJacket(9);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void leatherJacketTooLowTechLevelTest() {
+		// TechLevel should be 1-8
+		Equipment.Armor.LeatherJacket leatherJacket = new Equipment.Armor.LeatherJacket(0);
 	}
 	
 	@Test
-	public void shieldConstructorTest() {
-		Equipment.Shield buckler = new Equipment.Shield("Buckler", 25, 2.0, 2);
+	public void bucklerConstructorTest() {
+		Equipment.Shield.Buckler buckler = new Equipment.Shield.Buckler();
 		
 		assertEquals("Buckler", buckler.getDescription());
 		assertEquals(25, buckler.getValue());
 		assertEquals(2.0, buckler.getWeight());
-		assertEquals(2, buckler.getPassiveDefense());
+		assertEquals(1, buckler.getPassiveDefense());
+	}
+	
+	@Test
+	public void daggerConstructorTest() {
+		Equipment.Weapon.HandWeapon.Dagger dagger = new Equipment.Weapon.HandWeapon.Dagger();
+		
+		assertEquals("Dagger", dagger.getDescription());
+		assertEquals(20, dagger.getValue());
+		assertEquals(0.25, dagger.getWeight());
+		assertEquals(0, dagger.getMinimumStrength());
+		assertEquals(Equipment.Weapon.DAMAGE_TYPE_IMPALING, dagger.getDamageType());
 	}
 }
