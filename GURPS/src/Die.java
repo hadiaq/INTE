7ca572b5c131906a81a153f53f2e3b0642f522af
Erik Sandsten <eriksandsten12@gmail.com>
2015-11-09 15:51:54 +0100

    Lade till konstanterna MIN och MAX_VALUE istället för hårdkodade värden

--- GURPS/src/Die.java
+++ GURPS/src/Die.java
@@ -2,17 +2,19 @@
 import java.util.Random;
 
 public class Die {
+	public static int MIN_VALUE = 1;
+	public static int MAX_VALUE = 6;
 	
 	public int value;
-	
+
 	public Die() {
 		Random ran = new Random();
-		this.value = ran.nextInt(6) +1;
+		this.value = ran.nextInt(MAX_VALUE) +1;
 	}
 	
 	public Die(int seed) {
 		Random ran = new Random(seed);
-		this.value = ran.nextInt(6) +1;
+		this.value = ran.nextInt(MAX_VALUE) +1;
 	}
 	
 	public int getValue() {
@@ -21,12 +23,12 @@ public class Die {
 	
 	public static int roll() {
 		Random ran = new Random();
-		return ran.nextInt(6) +1;
+		return ran.nextInt(MAX_VALUE) +1;
 		
 	}
 	
 	public void seedRoll(int seed) {
 		Random ran = new Random();
-		value = ran.nextInt(6) +1;
+		value = ran.nextInt(MAX_VALUE) +1;
 	}
 }
