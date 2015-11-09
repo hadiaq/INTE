77eb6e65b20f1a46c3b6390ea8cfa662a86ec0c7
Erik Sandsten <eriksandsten12@gmail.com>
2015-11-09 15:58:35 +0100

    Fixade en ny DiceRoll-klass

---
+++ GURPS/src/DiceRoll.java
@@ -0,0 +1,32 @@
+public class DiceRoll {
+	// T.ex. 2d-5
+	private int numberOfDices; // 2
+	private int modifier; // -5
+	private Die[] dices;
+	private int value; // 2d-5
+
+	public DiceRoll(int numberOfDices, int modifier) {
+		this.numberOfDices = numberOfDices;
+		this.modifier = modifier;
+		dices = new Die[numberOfDices];
+
+		for (int i = 0; i < numberOfDices; i++) {
+			dices[i] = new Die();
+			value += dices[i].getValue();
+		}
+
+		value += modifier;
+	}
+
+	public int getNumberOfDices() {
+		return numberOfDices;
+	}
+
+	public int getModifier() {
+		return modifier;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
---
+++ GURPS/src/DiceRollTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DiceRollTest {
+
+	@Test
+	public void constructorTest() {
+		int numberOfDices = 3;
+		int modifier = -2;
+		int minimumPossibleValue = numberOfDices * Die.MIN_VALUE + modifier;
+		int maximumPossibleValue = numberOfDices * Die.MAX_VALUE + modifier;
+
+		DiceRoll diceRoll = new DiceRoll(numberOfDices, modifier);
+
+		assertEquals(diceRoll.getNumberOfDices(), numberOfDices);
+		assertEquals(diceRoll.getModifier(), modifier);
+		assertTrue(diceRoll.getValue() >= minimumPossibleValue && diceRoll.getValue() <= maximumPossibleValue);
+	}
+
+}
